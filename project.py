d0e4bf725a0b76628b1ca2769a91bbd9fc2b6688
Edward <contact@edw.dev>
2023-09-17 00:25:39 +0300

    Feat: implement api key getter function

--- project.py
+++ project.py
@@ -1,7 +1,26 @@
+from configparser import ConfigParser
 import sys
+
+
+def _get_api_key():
+    """
+    Get the API key from the config file
+
+    Expects a "secrets.ini" file in the same directory as this file with the following format:
+        [openweather]
+        api_key=<OPENWEATHER_API_KEY>
+
+    Returns:
+        str: The API key
+    """
+    config = ConfigParser()
+    config.read("secrets.ini")
+    return config["openweather"]["api_key"]
+
 
 def main():
     ...
+
 
 if __name__ == "__main__":
     main()
